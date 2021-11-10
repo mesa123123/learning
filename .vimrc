edb0edec7e80a390762abfe025698f17810837bc
bowmanpete <mesa123123@users.noreply.github.com>
2021-11-10 13:45:04 +1300

    Sessions End Wed 10 Nov 2021 13:45:04 NZDT

---
+++ .vimrc
@@ -0,0 +1,5 @@
+" Set up this workspace as a vimwiki diretory
+" let g:CURRENT_DIR = getcwd()
+let g:vimwiki_list = [{'path': './', 'syntax': 'markdown', 'ext':'.md'}]
+let g:CURRENT_DIR = vimwiki_list
+
---
+++ config.rb
@@ -0,0 +1,11 @@
+# Gollum Setup
+Gollum::Page.send :remove_const, :FORMAT_NAMES if defined? Gollum::Page::FORMAT_NAMES
+
+# Takes away all markups as a format
+Gollum::Markup.formats.clear
+
+Gollum::Markup.formats[:markdown] = {
+  :name => "Markdown",
+  :extensions => ['.md'],
+  :reverse_links => false
+}
