36cf6d616f68889255d3b27bcf544f2d94e6669c
Peter Bowman <bowmanpete123@gmail.com>
2022-01-20 11:50:22 +1300

    Update config.rb

--- config.rb
+++ config.rb
@@ -1,6 +1,9 @@
 # Gollum Setup
 Gollum::Page.send :remove_const, :FORMAT_NAMES if defined? Gollum::Page::FORMAT_NAMES
 
+# Change the default entry point
+Precious::App.set(:wiki_options, { :index_page => 'index' })
+
 # Takes away all markups as a format
 Gollum::Markup.formats.clear
 
